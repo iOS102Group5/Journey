37d34bd3766273c7ff6ba6af220b61f04b7f89ef
AbhijeetCherungottil1 <Abhijeet.Cherungottil1@marist.edu>
2025-11-05 21:24:37 -0500

    Initial App Setup

---
+++ Journey/Journey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Journey
+//
+//  Created by Abhijeet Cherungottil on 11/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
